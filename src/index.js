f25cc6298eaf207f765c706c07dbc3c487392c69
Luv Karakoti <luv94.karakoti@gmail.com>
2021-11-08 22:25:11 +0100

    Updating first page for Telegramska

--- src/index.js
+++ src/index.js
@@ -7,7 +7,19 @@ class App extends React.Component {
   render() {
     return (
       <Fragment>
-        <div class="heading1">Finally</div>
+        <div className="row">
+          <div>
+            <div className="heading1 text-white push-up absolute">
+              Telegramska
+            </div>
+          </div>
+          <div className="twelve columns">
+            <div className="heading3 text-white">Telegramska</div>
+            <div className="heading4 text-white">
+              Your basic Telegram Bot helper
+            </div>
+          </div>
+        </div>
       </Fragment>
     )
   }
